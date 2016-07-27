3f0b4db0770c68172bcc14f22a821b531cb53b05
Gabriel Mojica <niko.mojica@live.com>
2016-07-27 15:14:26 +1000

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name corporateDashboardApp
+ * @description
+ * # corporateDashboardApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('corporateDashboardApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name corporateDashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the corporateDashboardApp
+ */
+angular.module('corporateDashboardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
