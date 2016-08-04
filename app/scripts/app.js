68184ca4e4c4a6c666f44e70de67ff3fdc4edb85
Gabriel Mojica <niko.mojica@live.com>
2016-08-04 18:43:37 +1000

    feat: Change tab class on click

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,5 +8,16 @@
  *
  * Main module of the application.
  */
-angular
-  .module('corporateDashboardApp', []);
+var app = angular.module('corporateDashboardApp', []);
+
+app.controller('NavbarController', function() {
+    this.tab = 1;
+
+    this.selectTab = function(tab) {
+        this.tab = tab;
+    };
+
+    this.isSelected = function(tab) {
+        return this.tab === tab;
+    };
+});
